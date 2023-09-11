38ac431975b275f7358aa6a97a30869e4790061a
rizle00 <144658576+rizle00@users.noreply.github.com>
2023-09-11 17:42:55 +0900

    이클립스 첫 실습
    입출력

---
+++ WorkSpace/01.HelloWorld/src/HelloWorld.java
@@ -0,0 +1,14 @@
+	//<-주석. 기계가 컴파일 하지 않는 코드, 즉 인간이 보기 위한 설명.
+/*1. main : 메인 메소드라고하며 우리가 당분간 계속 해서 사용할 프로그램의 시작점
+  2. syso : 프로그램이 진행되는 중에 어떤 문자나 결과를 출력하기 위한 명령어
+  3. ctrl + space : 코드의 자동 완성.
+  4. ctrl + s 또는 ctrl + shift +s : 현재파일저장, 전체파일저장*/
+public class HelloWorld {
+       public static void main(String[] args) {
+    	  System.out.println("HelloWorld!KMJ");
+    	  System.out.println("My name is \"KMJ\"");
+    	  System.out.println("It's a \"very\"nice day");
+    	  
+    	  
+       }
+}
