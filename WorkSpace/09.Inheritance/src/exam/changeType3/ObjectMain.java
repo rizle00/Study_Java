1ab505f49fe1e24f3a609c74a811a106fdfe7187
rizle00 <144658576+rizle00@users.noreply.github.com>
2023-10-18 15:35:00 +0900

    typeChange

---
+++ WorkSpace/09.Inheritance/src/exam/changeType3/ObjectMain.java
@@ -0,0 +1,21 @@
+package exam.changeType3;
+
+public class ObjectMain {
+public static void main(String[] args) {
+	//자바의 모든 클래스는 Object를 통해 만들어졌다.
+	//Object obj = ? ; <=
+	// String : 문자열에 특성화 기능
+	
+	Object obj = "문자열";
+	String str = (String) obj;
+	System.out.println(str.length() + str);
+	obj = 10;
+	int num = (int) obj;
+	obj = new child();
+}
+	public void method(Object obj) {
+		if(obj instanceof String) {
+			
+		}
+	}
+}
